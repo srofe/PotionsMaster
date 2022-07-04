da0e9959302084f85d8fd687c0aadb91ab04eee7
Simon Rofe <simon.rofe@ieee.org>
2022-07-04 18:08:58 +1000

    Initial Commit

---
+++ PotionsMaster/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PotionsMaster
+//
+//  Created by Simon Rofe on 4/7/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PotionsMaster/PotionsMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PotionsMasterApp.swift
+//  PotionsMaster
+//
+//  Created by Simon Rofe on 4/7/2022.
+//
+
+import SwiftUI
+
+@main
+struct PotionsMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
