5c8c8acb65b209b2d917128cfa086db059040821
Simon Rofe <simon.rofe@ieee.org>
2022-07-04 22:05:22 +1000

    Add ingredients row view

---
+++ PotionsMaster/Views/IngredientRow.swift
@@ -0,0 +1,65 @@
+//
+//  IngredientRow.swift
+//  PotionsMaster
+//
+//  Created by Simon Rofe on 4/7/2022.
+//
+
+import SwiftUI
+
+struct IngredientRow: View {
+    @State private var ingredientFormIsPresented = false
+    let ingredient: Ingredient
+
+    var buttonImage: String {
+        ingredient.bought ? "circle.inset.filled" : "circle"
+    }
+
+    var body: some View {
+        HStack {
+            Button(action: openUpdateIngredient) {
+                Text("\(ingredient.quantity)")
+                    .bold()
+                    .padding(.horizontal, 4)
+                VStack(alignment: .leading) {
+                    Text(ingredient.title)
+                        .font(.headline)
+                    Text(ingredient.notes)
+                        .font(.subheadline)
+                }
+                .lineLimit(1)
+            }
+            .buttonStyle(.plain)
+            .disabled(ingredient.bought)
+            .sheet(isPresented: $ingredientFormIsPresented) {
+                IngredientFormView(ingredient: ingredient)
+            }
+            Spacer()
+            Button(action: toggleBought) {
+                Image(systemName: buttonImage)
+                    .resizable()
+                    .frame(width: 24, height: 24)
+            }
+        }
+    }
+}
+
+extension IngredientRow {
+    func openUpdateIngredient() {
+        ingredientFormIsPresented.toggle()
+    }
+
+    func toggleBought() {
+        // TODO: Toggle bought
+    }
+}
+
+struct IngredientRow_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            IngredientRow(ingredient: IngredientMock.ingredientsMock[0])
+            IngredientRow(ingredient: IngredientMock.boughtIngredientsMock[0])
+        }
+        .previewLayout(.sizeThatFits)
+    }
+}
